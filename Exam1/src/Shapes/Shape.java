245a41595c1d15f60e5abd1d4908401cc6d1fd75
Uri Alvarez <167371454+Uri-Alvarez@users.noreply.github.com>
2024-06-12 09:10:09 -0600

    Update Shape.java

--- Exam1/src/Shapes/Shape.java
+++ Exam1/src/Shapes/Shape.java
@@ -1,26 +1,5 @@
 package Shapes;
 
-/*
-public abstract class Shape implements ShapeInterface {
-    private int numberOfSides;
-
-    public Shape(int numberOfSides) {
-        this.numberOfSides = numberOfSides;
-    }
-
-    @Override
-    public int getNumberOfSides() {
-        return numberOfSides;
-    }
-
-    @Override
-    public abstract double getArea();
-    
-    @Override
-    public abstract double getPerimeter();
-}
-*/
-
 public abstract class Shape implements ShapeInterface {
 	
 }
